85137cb23c84ee6f497afdf9d7691619ddc383ba
Dreamwave7 <dima190179@gmail.com>
2023-05-24 01:52:39 +0300

    п

--- seed.py
+++ seed.py
@@ -6,7 +6,7 @@ record1 = Record(description="Take tables")
 record2 = Record(description="Buy cigarettes")
 record3 = Record(description = "Buy meat")
 
-not11e =Notes(name = "Weekend", records =[record1,record2,record3],tags= [tag,])
+not11e =Notes(name = "Weekend", records =[record1,record2,record3],tags= [tag,]).save()
 
 
 notes = Notes.objects()
