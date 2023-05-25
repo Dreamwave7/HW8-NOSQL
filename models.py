bcbd2a571c7db8e9b21cab5268b9ce6c470a345e
Dreamwave7 <dima190179@gmail.com>
2023-05-25 21:48:09 +0300

    sd

--- models.py
+++ models.py
@@ -1,34 +1,23 @@
 from mongoengine import connect
-from mongoengine import EmbeddedDocument, Document
+from mongoengine import EmbeddedDocument, Document, ReferenceField
 from mongoengine.fields import BooleanField, DateTimeField,EmbeddedDocumentField, ListField,StringField
-import datetime
 
-connect(host="mongodb+srv://guest:guest@dreamwave.dhurwpi.mongodb.net/test?retryWrites=true&w=majority")
+connect(host="mongodb+srv://guest:guest@dreamwave.dhurwpi.mongodb.net/module8?retryWrites=true&w=majority")
 
-
-class Tag(EmbeddedDocument):
-    name = StringField()
-
-class Record(EmbeddedDocument):
+class Authors(Document):
+    fullname = StringField()
+    born_date = StringField()
+    born_location = StringField()
     description = StringField()
-    done = BooleanField()
+    meta = {"allow_inheritance": True}
+    
 
-class Notes(Document):
-    name = StringField()
-    created = DateTimeField(default=datetime.datetime.now())
-    records = ListField(EmbeddedDocumentField(Record))
-    tags = ListField(EmbeddedDocumentField(Tag))
+class Quotes(Document):
+    tags = ListField()
+    author = ReferenceField(Authors)
+    quote = StringField()
     meta = {"allow_inheritance": True}
 
-
-
-
-
-
-
-
-
-#mongodb+srv://guest:guest@dreamwave.dhurwpi.mongodb.net/?retryWrites=true&w=majority
 
 
 
--- seed.py
+++ seed.py
@@ -1,31 +1,38 @@
 from models import *
 import pprint
-tag = Tag(name="Weekends")
+import json
 
-record1 = Record(description="Take tables")
-record2 = Record(description="Buy cigarettes")
-record3 = Record(description = "Buy meat")
-
-not11e =Notes(name = "Weekend", records =[record1,record2,record3],tags= [tag,]).save()
+dict_authors = {}
 
 
-notes = Notes.objects()
-for note in notes:
-    pprint.pprint(note.to_mongo().to_dict())
+def seed_authors():
+    with open("authors.json") as file:
+        authors = json.load(file)
 
+    for i in authors:
+        author = Authors(
+            fullname = i["fullname"],
+            born_date = i["born_date"],
+            born_location = i["born_location"],
+            description = i["description"]
+            )
+        dict_authors[i["fullname"]] = author
+        author.save()
 
+def seed_quotes():
+    with open("quotes.json", encoding="utf8") as file:
+        quotes = json.load(file)
+    for i in quotes:
+        author = i["author"]
+        quote = Quotes(
+            tags = i["tags"],
+            author = dict_authors[author],
+            quote = i["quote"]
+            )
+        quote.save()
 
-
-
-
-
-
-
-
-
-
-
-
+seed_authors()
+seed_quotes()
 
 
 
