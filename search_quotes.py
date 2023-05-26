d185f4b31326ff9858e2b7e8ebce0a357e40f2ac
Dreamwave7 <dima190179@gmail.com>
2023-05-26 14:56:53 +0300

    search

--- search_quotes.py
+++ search_quotes.py
@@ -1,5 +1,4 @@
 from models import *
-import pprint
 from colorit import *
 
 
@@ -17,18 +16,39 @@ def main():
                 author_id = quot.author.id
                 author = Authors.objects(id= author_id)[0]
                 print(color(f'''
-                \n
                 Author = {author.fullname}.
-                Quotes = {quot.quote}\n
+                Quotes = {quot.quote}
                 ''',Colors.blue
                 ))
 
+        elif command.startswith("tags:"):
+            search = command.split(":")[1].split(",")
+            quotes = Quotes.objects(tags__in =search)
+
+            for quote in quotes:
+                author_id = quote.author.fullname
+                print(color(f'''
+                Author = {author_id}.
+                Quotes = {quote.quote}
+                ''',Colors.blue))
+
+        elif command.startswith("name:"):
+            search = command.split(":")[1].strip()
+            author_id = Authors.objects(fullname = search)[0].id
+            author_name = Authors.objects(fullname = search)[0].fullname
+            list_quotes = Quotes.objects(author = author_id)
+            for quote in list_quotes:
+                print(color(f"""
+                Author = {author_name}.
+                Quotes = {quote.quote}
+                """,Colors.blue))
 
         else:
             print(color("Wrong command",Colors.red))
 
 
 if __name__ == "__main__":
-    main()
-    # id = Authors.objects(fullname="Albert Einstein").first()
-    # print(id.id)
+    try:
+        main()
+    except Exception:
+        print(color("Something went wrong, check ur values",Colors.red))
